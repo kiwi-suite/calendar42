18c0940ac0f17ddcd190678a68dbf09cb50144c5
luchaos <lcs@luchaos.at>
2017-02-20 13:47:15 +0100

    translations

---
+++ data/language/de-DE.php
@@ -0,0 +1,41 @@
+<?php
+
+return [
+
+    'title.calendar.list' => 'Kalender',
+
+    'title.calendar' => 'Kalender',
+    'title.event'    => 'Termin',
+
+    'title.calendar-new' => 'Neuer Kalender',
+    'title.calendar-list' => 'Kalender',
+
+    'title.event-new'    => 'Neuer Termin',
+    'title.event-list'    => 'Termine',
+
+    'label.calendar' => 'Kalender',
+    'label.title'    => 'Titel',
+    'label.color'    => 'Farbe',
+    'label.handle'   => 'Handle',
+    'label.location' => 'Ort',
+    'label.info'     => 'Information',
+    'label.start'    => 'Beginn',
+    'label.end'      => 'Ende',
+    'label.all-day'  => 'Als Ganztagstermin anzeigen. Achtung: Die Uhrzeit des Enddatums ist exklusiv. iCal Exporte beinhalten exaktes Datum und Uhrzeit um Konflikte bei Zeitzonen zu vermeiden.',
+
+    'button.today'         => 'Heute',
+    'button.day'           => 'Tag',
+    'button.week'          => 'Woche',
+    'button.month'         => 'Monat',
+    'button.event-list'    => 'Terminliste',
+    'button.event-add'     => 'Termin hinzufügen',
+    'button.calendar-list' => 'Kalenderliste',
+    'button.calendar-add'  => 'Kalendar hinzufügen',
+    'button.calendar-ical' => 'iCal Herunterladen',
+
+    'toaster.calendar.create.title.success'   => 'Kalender wurde erstellt',
+    'toaster.calendar.create.message.success' => ' ',
+
+    'toaster.calendar.create.title.error'   => 'Kalender konnte nicht erstellt werden',
+    'toaster.calendar.create.message.error' => ' ',
+];
--- src/Form/Event/EditForm.php
+++ src/Form/Event/EditForm.php
@@ -10,7 +10,6 @@
 namespace Calendar42\Form\Event;
 
 use Admin42\FormElements\Form;
-use Admin42\FormElements\Link;
 
 class EditForm extends Form
 {
@@ -19,60 +18,78 @@ class EditForm extends Form
      */
     public function init()
     {
-        $this->add([
-            'name' => 'csrf',
-            'type' => 'csrf',
-        ]);
+        $this->add(
+            [
+                'name' => 'csrf',
+                'type' => 'csrf',
+            ]
+        );
 
-        $this->add([
-            'name'     => 'title',
-            'type'     => 'text',
-            'label'    => 'label.title',
-            'required' => true,
-        ]);
+        $this->add(
+            [
+                'name'     => 'title',
+                'type'     => 'text',
+                'label'    => 'label.title',
+                'required' => true,
+            ]
+        );
 
-        $this->add([
-            'type' => 'calendar',
-            'name' => 'calendarId',
-            'label' => 'label.calendar',
-            'required' => true,
-        ]);
+        $this->add(
+            [
+                'type'     => 'calendar',
+                'name'     => 'calendarId',
+                'label'    => 'label.calendar',
+                'required' => true,
+            ]
+        );
 
-        $this->add([
-            'name'     => 'start',
-            'type'     => 'dateTime',
-            'label'    => 'Start',
-            'required' => true,
-        ]);
+        $this->add(
+            [
+                'name'     => 'start',
+                'type'     => 'dateTime',
+                'label'    => 'label.start',
+                'required' => true,
+            ]
+        );
 
-        $this->add([
-            'name'  => 'end',
-            'type'  => 'dateTime',
-            'label' => 'End',
-        ]);
+        $this->add(
+            [
+                'name'  => 'end',
+                'type'  => 'dateTime',
+                'label' => 'label.end',
+            ]
+        );
 
-        $this->add([
-            'name'  => 'allDay',
-            'type'  => 'checkbox',
-            'label' => 'label.all-day',
-        ]);
+        $this->add(
+            [
+                'name'  => 'allDay',
+                'type'  => 'checkbox',
+                'label' => 'label.all-day',
+            ]
+        );
 
-        $this->add([
-            'name'  => 'location',
-            'type'  => 'text',
-            'label' => 'label.location',
-        ]);
+        $this->add(
+            [
+                'name'  => 'location',
+                'type'  => 'text',
+                'label' => 'label.location',
+            ]
+        );
 
-        $this->add([
-            'name'  => 'info',
-            'type'  => 'textarea',
-            'label' => 'label.info',
-        ]);
+        $this->add(
+            [
+                'name'  => 'info',
+                'type'  => 'textarea',
+                'label' => 'label.info',
+            ]
+        );
 
-        $this->add([
-            'type' => 'link',
-            'name' => "linkId",
-            'label' => 'label.link'
-        ]);
+        $this->add(
+            [
+                'type'  => 'link',
+                'name'  => "linkId",
+                'label' => 'label.link'
+            ]
+        );
     }
 }
